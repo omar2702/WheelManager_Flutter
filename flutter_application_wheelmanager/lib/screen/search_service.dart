173147131843305fb3ef51d8a956728b33f91e12
Maro <omaralonso27@gmail.com>
2021-05-28 14:24:29 -0500

    feat: Navigation to the Scaffolds

---
+++ flutter_application_wheelmanager/lib/screen/search_service.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchService extends StatelessWidget {
+  const SearchService({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buscar Servicio'),
+      ),
+    );
+  }
+}
