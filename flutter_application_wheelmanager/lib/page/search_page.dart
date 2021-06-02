bdeebe1db6ea1f8e610a55743e62452895703214
Maro <omaralonso27@gmail.com>
2021-06-02 15:26:14 -0500

    hot: search_page

--- flutter_application_wheelmanager/lib/page/search_page.dart
+++ flutter_application_wheelmanager/lib/page/search_page.dart
@@ -1,51 +1,103 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_wheelmanager/screen/search_bussines.dart';
+import 'package:flutter_application_wheelmanager/colors/color_filter.dart';
 import 'package:flutter_application_wheelmanager/screen/search_product.dart';
+import 'package:flutter_application_wheelmanager/screen/search_bussines.dart';
 import 'package:flutter_application_wheelmanager/screen/search_service.dart';
 
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // ignore: deprecated_member_use
-          RaisedButton(
-            shape: StadiumBorder(),
-            child: Text('Buscar Empresa'),
-            color: Colors.orangeAccent[200],
-            textColor: Colors.white,
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SearchBusiness()));
-            },
-          ),
-          // ignore: deprecated_member_use
-          RaisedButton(
-            shape: StadiumBorder(),
-            child: Text('Buscar Producto'),
-            color: Colors.orangeAccent[200],
-            textColor: Colors.white,
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SearchProduct()));
-            },
-          ),
-          // ignore: deprecated_member_use
-          RaisedButton(
-            shape: StadiumBorder(),
-            child: Text('Buscar Servicio'),
-            color: Colors.orangeAccent[200],
-            textColor: Colors.white,
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SearchService()));
-            },
-          )
-        ],
-      )),
-    );
+        body: ListView(
+      padding: EdgeInsets.all(20.0),
+      children: <Widget>[
+        _cardProduct(context),
+        SizedBox(
+          height: 30.0,
+        ),
+        _cardService(context),
+        SizedBox(
+          height: 30.0,
+        ),
+        _cardBussines(context),
+        SizedBox(
+          height: 30.0,
+        )
+      ],
+    ));
   }
+
+  Widget _cardProduct(context) => Card(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage('assets/bicicle.png'),
+              child: InkWell(onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SearchProduct()));
+              }),
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Buscar Productos',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 24),
+            )
+          ],
+        ),
+      );
+
+  Widget _cardService(context) => Card(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage('assets/service.jpg'),
+              colorFilter: ColorFilters.greyscale,
+              child: InkWell(onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SearchService()));
+              }),
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Buscar Servicios',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 24),
+            )
+          ],
+        ),
+      );
+
+  Widget _cardBussines(context) => Card(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage('assets/empresa.jpg'),
+              colorFilter: ColorFilters.greyscale,
+              child: InkWell(onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SearchBusiness()));
+              }),
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Buscar Empresas',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 24),
+            )
+          ],
+        ),
+      );
 }
