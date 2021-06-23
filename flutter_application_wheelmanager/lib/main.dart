318d197098026374f490ee18e9052285047b2fc2
Sergio Alva <49848146+Setsuke@users.noreply.github.com>
2021-06-23 00:15:10 -0500

    edit main to firebase

--- flutter_application_wheelmanager/lib/main.dart
+++ flutter_application_wheelmanager/lib/main.dart
@@ -1,32 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_wheelmanager/User/bloc/bloc_user.dart';
+import 'package:flutter_application_wheelmanager/User/bloc/sign_in_screen.dart';
 import 'package:flutter_application_wheelmanager/login/login_page.dart';
 //import 'package:flutter_application_wheelmanager/home.dart';
 import 'package:flutter_application_wheelmanager/model/user_preferences.dart';
 import 'package:flutter/services.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
-Future main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await UserPreferences.init();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Wheel Manager',
-      theme: ThemeData(
-        primaryColor: Colors.orange,
-        // ignore: deprecated_member_use
-        accentColor: Colors.orangeAccent[200],
-      ),
-      home: Login(),
-    );
+    return BlocProvider(
+        child: MaterialApp(
+          title: 'Wheel Manager',
+          home: SignInScreen(),
+        ),
+        bloc: UserBloc());
+
+    //title: 'Wheel Manager',
+    //theme: ThemeData(
+    // primaryColor: Colors.orange,
+    // ignore: deprecated_member_use
+    // accentColor: Colors.orangeAccent[200],
+    // ),
+    //  home: Login(),
+    //  );
   }
 }
