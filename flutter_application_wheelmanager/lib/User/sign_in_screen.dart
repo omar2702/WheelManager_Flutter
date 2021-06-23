97e3c54df6867ed3888df4acb3a94e4a0084b751
Sergio Alva <49848146+Setsuke@users.noreply.github.com>
2021-06-23 00:10:20 -0500

    sign in screen

---
+++ flutter_application_wheelmanager/lib/User/sign_in_screen.dart
@@ -0,0 +1,68 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_wheelmanager/User/bloc/bloc_user.dart';
+import 'package:flutter_application_wheelmanager/global_widgets/button_green.dart';
+import 'package:flutter_application_wheelmanager/global_widgets/gradient_back.dart';
+import 'package:flutter_application_wheelmanager/home.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SignInScreen();
+  }
+}
+
+class _SignInScreen extends State<SignInScreen> {
+  UserBloc userBloc;
+
+  @override
+  Widget build(BuildContext context) {
+    userBloc = BlocProvider.of(context);
+    return _handleCurrentSession();
+  }
+
+  Widget _handleCurrentSession() {
+    return StreamBuilder(
+      stream: userBloc.authStatus,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData || snapshot.hasError) {
+          return signInGoogleUI();
+        } else {
+          return HomePage();
+        }
+      },
+    );
+  }
+
+  Widget signInGoogleUI() {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          GradientBack("", null),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome \n This App is For You",
+                style: TextStyle(
+                    fontSize: 37.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              ButtonGreen(
+                text: "Login with Gmail",
+                onPressed: () {
+                  userBloc.signIn();
+                },
+                width: 300.0,
+                height: 50.0,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
